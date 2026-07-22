39a8a916b8bf0b86696c18a6e0e07924933f25db
mlamamra <mlamamra@dawan.fr>
2026-07-22 13:48:12 +0200

    j3: module

--- bank.py
+++ bank.py
@@ -1,11 +1,11 @@
-def withdraw(account: dict, amount: float):
+def withdraw(account: dict, amount: float) -> float:
   if amount < 0:
     print(f"Transaction refusée: {amount} négatif")
   elif amount > account["balance"] + account["overdraft"]:
     print(f"Transaction refusée: {amount} fonds insuffisants")
   else:
     account["balance"] -= amount
-    print(f"Transaction acceptée")
+    return account
 
 if __name__ == "__main__":
     print("coucou")
